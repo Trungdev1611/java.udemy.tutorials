5286e4f2a6cb6b19b298b23cac70706d51fd2e15
Trungdev1611 <trungdev1611@gmail>
2023-08-23 09:42:25 +0700

    getData from token and validate token

--- src/main/java/com/example/udemyspring/security/JWTTokenProvider.java
+++ src/main/java/com/example/udemyspring/security/JWTTokenProvider.java
@@ -4,11 +4,19 @@ import java.security.Key;
 import java.util.Date;
 import java.util.Base64.Decoder;
 
+import org.modelmapper.internal.asm.tree.UnsupportedClassVersionException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import com.example.udemyspring.exception.ApiException;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -26,6 +34,7 @@ public class JWTTokenProvider {
     public String generateToken(Authentication authentication) {
         String username = authentication.getName();
 
+        System.out.println("generateToken::::" + username);
         Date currentDate = new Date();
 
         // end date - thời gian hết hạn kể từ lúc tạo token
@@ -45,5 +54,36 @@ public class JWTTokenProvider {
     // khi truyền vào signWith
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    // get Username from token
+    public String getUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        String username = claims.getSubject();
+        return username;
+    }
+
+    // validate jwt token
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (MalformedJwtException ex) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "InvalidJwt", 401);
+        } catch (ExpiredJwtException ex) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Expired Jwt token", 401);
+        } catch (UnsupportedJwtException ex) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Unsupport JWT token", 401);
+        } catch (IllegalArgumentException ex) {
+            throw new ApiException(HttpStatus.BAD_REQUEST, "JWT claims string is empty", 401);
+        }
+
     }
 }
